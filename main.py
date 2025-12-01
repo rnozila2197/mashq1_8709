afa2e90f4e8d4382ba928440a8fbd43b5765bccb
rnozila2197 <yjamshid2197@gmail.com>
2025-12-01 15:34:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+# 11
+matn = "abcdefghijk"
+print(len(matn[0:10:3]))
+
+# 12
+matn = "PYTHONCODE"
+print(slice(matn[0:9:2]))
+
+# 13
+matn = "abracadabra"
+print(slice(matn[1:4:1]))
+
+# 14
+matn = "helloslice"
+print(matn[2:7])
+
+# 15
+matn = "MissionComplete"
+print(matn[7:])
+
+# 16
+son = "9876543210"
+print(slice(son[0:6:1]))
+
+# 17
+matn = "dictionary"
+print(matn[3:6])
+
+# 18
+matn = "PowerfulString"
+print(matn[5:])
+
+# 19
+matn = ("abcdefghijk")
+print(matn[-9:2])
+
+# 20
+matn = "JKLMNOPRST"
+print(matn[-10:-5])
+
+# 21
+s = "transportation"
+print(s[0:9])
+
+# 22
+s = "demonstratation"
+print(slice(s))
+
+# 23
+s = "2468013579"
+print(slice(s[1:9:1]))
+
+# 24
+s = "environment"
+print(s[3:7])
+
+# 25
+s = ("strawberry")
+print(s[-1:-4:-1])
+
+# 26
+s = "programmer"
+print(slice(s[3:7]))
+
+# 27
+s = "abcdefghijklmnop"
+print(s[3:6])
+
+# 28
+s = ("ZXCVBNMASDFGHJ")
+print(slice(s))
